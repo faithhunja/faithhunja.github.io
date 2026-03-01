61aacc0fabfea1be69c512a94e272405b60ed9fd
faithhunja <faithhunja17@gmail.com>
2026-03-01 23:30:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Hello, I'm **Faith Hunja**. I am a **Data Engineer**.
+
+My core tools include:
+
+- SQL
+- Microsoft SQL Server
+- Python
+- Microsoft Fabric
+- Power BI
+
+I also work on cloud-based solutions on **Microsoft Azure**.
+
+Let's connect!
+
+[![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/faithhunja)  
---
+++ src/consts.ts
@@ -0,0 +1,44 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "Faith Hunja",
+  // EMAIL: "faithhunja17{at}gmail{dot}com",
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Faith Hunja's portfolio website.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of my articles.",
+};
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "Where I have worked and what I have done.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION: "A collection of my projects.",
+};
+
+export const SOCIALS: Socials = [
+  { 
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/faithhunja",
+  },
+  { 
+    NAME: "github",
+    HREF: "https://github.com/faithhunja"
+  },
+  { 
+    NAME: "orcid",
+    HREF: "https://orcid.org/0000-0001-5747-0247"
+  }
+];
